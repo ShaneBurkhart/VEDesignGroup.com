f88cbfcd667c86aab10b3060e60c782f77ad6f19
Shane Burkhart <shaneburkhart@gmail.com>
2017-04-12 15:33:30 -0500

    Cleanup

--- public/head.php
+++ public/head.php
@@ -3,24 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cantarell:400,700|Fjalla+One" rel="stylesheet">
     <link href="/wp-content/themes/custom/style.css" rel="stylesheet">
 
     <script
       src="https://code.jquery.com/jquery-2.2.4.min.js"
       integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
       crossorigin="anonymous"></script>
-    <script src="/wp-content/themes/custom/assets/js/handlebars-v4.0.5.js"></script>
 
-    <meta name="og:image" content="<?php the_field('banner_image'); ?>">
-
-    <title><?php
-        if (in_category('recipe', get_the_ID())) {
-            echo wp_title('', false)." Recipes";
-        } else {
-            echo wp_title('', false);
-        }
-    ?></title>
+    <title><?php echo wp_title('', false); ?></title>
 
     <?php wp_head(); ?>
 </head>
