dd9896bbe69f466d66278ac9986cdb3f422ff5c5
Shane Burkhart <shaneburkhart@gmail.com>
2017-06-28 19:57:16 -0500

    Add mobile nav.

--- public/header.php
+++ public/header.php
@@ -7,7 +7,7 @@
     <div class="large-container">
         <div class="full">
             <a id="site-logo" href="/"></a>
-            <ul>
+            <ul id="desktop-links">
                 <?php
                     foreach ($site_header_menu as $item) {
                         $item_class = '';
@@ -16,6 +16,56 @@
                         <li><a class="<?php echo $item_class; ?>" href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a></li>
                 <?php } ?>
             </ul>
+            <ul id="mobile-links">
+                <li><a href="jovascript:void();"><i class="fa fa-bars"></i></a></li>
+            </ul>
         </div>
     </div>
 </header>
+<div id="mobile-nav">
+    <div class="close">
+        <a href="javascript:void();"><i class="fa fa-times"></i></a>
+    </div>
+    <ul>
+        <?php
+            foreach ($site_header_menu as $item) {
+                $item_class = '';
+                if (trailingslashit($item->url) == get_permalink()) $item_class = 'selected';
+        ?>
+                <li><a class="<?php echo $item_class; ?>" href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a></li>
+        <?php } ?>
+    </ul>
+</div>
+
+<script>
+    $(document).ready(function () {
+        var $mobileDrawer = $('#mobile-nav');
+        var $drawerButton = $('#mobile-links li a');
+        var $closeButton = $mobileDrawer.find('.close');
+        var isOpen = false;
+
+        $drawerButton.click(function (e) {
+            e.preventDefault();
+            if (isOpen) return;
+
+            $mobileDrawer.css({
+                'left': 'auto',
+                'right': '0',
+            });
+
+            isOpen = true;
+        });
+
+        $closeButton.click(function (e) {
+            e.preventDefault();
+            if (!isOpen) return;
+
+            $mobileDrawer.css({
+                'left': '-100%',
+                'right': 'auto',
+            });
+
+            isOpen = false;
+        });
+    });
+</script>
